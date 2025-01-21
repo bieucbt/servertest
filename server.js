adcaa23b7862a5500c281cbe7bd69e65e391e98c
bieucbt <bieucbt@gmail.com>
2025-01-21 16:53:25 +0700

    test

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const mongoose = require('mongoose');
+
 
 app.get('/', (req, res) => {
   res.send('my server333')
 })
 
-app.listen(process.env.PORT, () => {
-  console.log('listening on port ', process.env.PORT)
-})
+// Kết nối MongoDB
+mongoose.connect(process.env.MONGDB_URL)
+  .then(() => {
+    console.log('Kết nối MongoDB thành công!');
+    app.listen(process.env.PORT || 3000, () => {
+      console.log('Server is running on port', process.env.PORT || 3000);
+    });
+  })
+  .catch((err) => {
+    console.error('Lỗi kết nối MongoDB:', err);
+  });
