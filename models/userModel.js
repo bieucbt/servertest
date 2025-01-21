633f200fbcaa05ee13cea72d668622f08a440724
bieucbt <bieucbt@gmail.com>
2025-01-21 17:07:22 +0700

    update

---
+++ models/userModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  email: {type: String, require: true, unique: true },
+  password: {type: String, require: true},
+  isAdmin: {type: Boolean, default: false}  
+})
+
+module.exports = mongoose.model('User', userSchema)
