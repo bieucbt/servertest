5da64b16b40273bc35be48c8aa39e1e060c7505b
bieucbt <bieucbt@gmail.com>
2025-01-21 16:42:44 +0700

    update

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-
+require('dotenv').config()
 
 app.get('/', (req, res) => {
   res.send('my server')
 })
 
-app.listen(3000, () => {
-  console.log('listening on port 3000')
+app.listen(process.env.PORT, () => {
+  console.log('listening on port ', process.env.PORT)
 })
