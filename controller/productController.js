6635fc5f64ce70a04cb99fb6f9f45ce086ec12f4
bieucbtTest <biecbt@gmail.com>
2025-02-04 21:40:21 +0700

    update

--- controller/productController.js
+++ controller/productController.js
@@ -10,6 +10,16 @@ const getAllProducts = async (req, res) => {
   }
 }
 
+const getOneProduct = async (req, res) => {
+  const { id } = req.params
+  try {
+    const product = await productModels.findById(id)
+    return res.status(200).json(product)
+  } catch (err) {
+    return res.status(404).json({ message: 'không tìm thấy sản phẩm' })
+  }
+}
+
 const createProduct = async (req, res) => {
   try {
     const product = await productModels.create({ ...req.body, img: req.file.filename })
@@ -19,8 +29,31 @@ const createProduct = async (req, res) => {
   }
 }
 
+const deleteProduct = async (req, res) => {
+  const { id } = req.params
+  try {
+    const product = await productModels.findByIdAndDelete(id)
+    return res.status(204).json(product)
+  } catch (err) {
+    return res.status(400).json({ message: err })
+  }
+}
+
+const updateProduct = async (req, res) => {
+  const { id } = req.params
+  try {
+    const product = await productModels.findById(id)
+    if (product)
+      return res.status(200).json(product)
+  } catch (err) {
+    return res.status(400).json({ message: err.message })
+  }
+}
 
 module.exports = {
   getAllProducts,
-  createProduct
+  createProduct,
+  deleteProduct,
+  getOneProduct,
+  updateProduct
 }
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,11 +1,18 @@
 const express = require('express')
-const { getAllProducts, createProduct } = require('../controller/productController')
+const { getAllProducts, createProduct, deleteProduct, getOneProduct,
+    updateProduct
+} = require('../controller/productController')
 const router = express()
 const upload = require('../middleware/uploadMiddleware')
 
 router.get('/', getAllProducts)
 
-// Áp dụng middleware Multer
+router.get('/:id', getOneProduct)
+
 router.post('/', upload.single('img'), createProduct);
+
+router.patch('/:id', upload.single('img'), updateProduct);
+
+router.delete('/:id', deleteProduct)
 
 module.exports = router
